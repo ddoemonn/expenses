7ea23ef07f4c32207fd1b042403d015d1d647417
ddoemonn <ozergokalpsezer@gmail.com>
2024-06-05 22:15:41 +0300

    ADD: component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {useState} from "react";
 
 
 function App() {
+    const [totalSpent, setTotalSpent] = useState(0)
 
   return (
-    <>
+      <Card className="w-[350px] mx-auto">
+        <CardHeader>
+          <CardTitle>Total Spent</CardTitle>
+          <CardDescription>The total amount you've spent </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{totalSpent}</p>
+        </CardContent>
 
-    </>
+      </Card>
+
   )
 }
 
