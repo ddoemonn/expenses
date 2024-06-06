abe8f4253538be92dffb6b5aa366eaa7d9dc6957
ddoemonn <ozergokalpsezer@gmail.com>
2024-06-06 19:15:07 +0300

    UPDATE: total spent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,35 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 function App() {
     const [totalSpent, setTotalSpent] = useState(0)
 
-  return (
-      <Card className="w-[350px] mx-auto">
-        <CardHeader>
-          <CardTitle>Total Spent</CardTitle>
-          <CardDescription>The total amount you've spent </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>{totalSpent}</p>
-        </CardContent>
+    // Fetch total spent from the server
+    useEffect(() => {
+      async function fetchTotalSpent() {
+        const response = await fetch("/api/expenses/total-spent")
+        const data = await response.json()
+        setTotalSpent(data.totalSpent)
+      }
+      fetchTotalSpent();
+    }, [])
 
-      </Card>
+    return (
+        <Card className="w-[350px] mx-auto">
+          <CardHeader>
+            <CardTitle>Total Spent</CardTitle>
+            <CardDescription>The total amount you've spent </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>{totalSpent}</p>
+          </CardContent>
 
-  )
+        </Card>
+
+    )
 }
 
 export default App
